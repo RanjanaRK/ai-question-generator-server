250120ba688a22dbecf84b09492a082919fe43d1
RanjanaRK <ranjanakumari.me@gmail.com>
2025-12-13 21:34:41 +0530

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import app from "./server";
+import cors from "cors";
+import express from "express";
+
+app.use(cors());
+app.use(express());
 
 app.listen(8000, () => {
   console.log("server is running okay");
