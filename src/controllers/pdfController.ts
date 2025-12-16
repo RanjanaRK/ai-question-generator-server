0cc8106bc438c021d1c39fbd19e23d0f76d5ad89
RanjanaRK <ranjanakumari.me@gmail.com>
2025-12-16 23:26:08 +0530

    .

--- src/controllers/pdfController.ts
+++ src/controllers/pdfController.ts
@@ -1,4 +1,18 @@
-export const uploadPdf = async () => {
+import { Request, Response } from "express";
+import pdf from 'pdf-parse';
+import fs from "fs";
+
+export const uploadPdf = async (req: Request, res: Response) => {
   try {
+    if (!req.file || !req.file.path) {
+      return res.status(400).json({ message: "No file uploaded" });
+    }
+    const dataBuffer = fs.readFileSync(req.file?.path);
+
+    const data = await pdf(dataBuffer);
+    // res.json({
+    //   message: "PDF uploaded successfully",
+    //   text: data.text.substring(0, 500), 
+    // });
   } catch (error) {}
 };
