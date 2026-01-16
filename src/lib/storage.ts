d1cc1565148d36c1bf267e6e3a23c2466089cb66
RanjanaRK <ranjanakumari.me@gmail.com>
2026-01-16 16:40:23 +0530

    Update storage.ts

---
+++ src/lib/storage.ts
@@ -0,0 +1,23 @@
+import { supabase } from "./supabase";
+import fs from "fs";
+
+export const uploadPdfStorage = async (
+  pdfs: string,
+  localFilePath: string,
+  storagePath: string,
+  contentType: string
+) => {
+  const fileBuffer = fs.readFileSync(localFilePath);
+
+  try {
+    const { data, error } = await supabase.storage
+      .from(pdfs)
+      .upload(storagePath, fileBuffer, {
+        contentType,
+        upsert: false,
+      });
+    if (error) throw new Error(error.message);
+
+    return data;
+  } catch (error) {}
+};
