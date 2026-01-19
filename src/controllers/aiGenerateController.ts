feaa81e7ec7ca0ad8e1f590a3776b7e263cb1746
RanjanaRK <ranjanakumari.me@gmail.com>
2026-01-19 21:57:43 +0530

    Update aiGenerateController.ts

--- src/controllers/aiGenerateController.ts
+++ src/controllers/aiGenerateController.ts
@@ -2,5 +2,10 @@ import { Request, Response } from "express";
 
 export const mcqGenerate = async (req: Request, res: Response) => {
   try {
+    const { pdfId } = req.body;
+
+    if (!pdfId) {
+      return res.status(400).json({ error: "pdf is required" });
+    }
   } catch (error) {}
 };
