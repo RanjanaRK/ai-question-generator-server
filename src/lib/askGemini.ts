6afa0b28332f6ca762d87d851b0304a1b5e8aaa6
RanjanaRK <ranjanakumari.me@gmail.com>
2026-01-22 17:17:53 +0530

    askGemini

--- src/lib/askGemini.ts
+++ src/lib/askGemini.ts
@@ -1,6 +1,6 @@
 import { ai } from "./gemini";
 
-export const geminiAsk = async (content: string) => {
+export const askGemini = async (content: string) => {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
     contents: content,
