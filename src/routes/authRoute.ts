7797f5311fd519c3c8e83d8617b91693a4ced635
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-07 19:48:19 +0530

    Update authRoute.ts

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { register } from "../controllers/auth/registerController";
 import { login } from "../controllers/auth/loginController";
+import { logout } from "../controllers/auth/logoutController";
 
 const authRouter = Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.post("/logout", logout);
 
 export default authRouter;
