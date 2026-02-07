564df05b0e682ae744c79aed0000c3e8ca34cfdf
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-07 23:08:14 +0530

    getQaByPdf

--- src/controllers/getAiDataController.ts
+++ src/controllers/getAiDataController.ts
@@ -24,3 +24,27 @@ export const getMcqsByPdf = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Failed to fetch MCQs" });
   }
 };
+
+export const getQaByPdf = async (req: Request, res: Response) => {
+  try {
+    const pdfId = req.query.pdfId as string;
+
+    const qaSets = await prisma.qaSet.findMany({
+      where: { pdfId },
+      include: {
+        items: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    res.json({
+      success: true,
+      data: qaSets,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch MCQs" });
+  }
+};
--- src/routes/aiRoutes.ts
+++ src/routes/aiRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { generateMcq, generateQA } from "../controllers/aiGenerateController";
-import { getMcqsByPdf } from "../controllers/getAiDataController";
+import { getMcqsByPdf, getQaByPdf } from "../controllers/getAiDataController";
 import { requireAuth } from "../middlewares/requireAuth";
 
 const airouter = Router();
@@ -10,5 +10,7 @@ airouter.post("/generate/mcq", generateMcq);
 airouter.post("/generate/qa", generateQA);
 
 airouter.get("/mcq/:pdfId", requireAuth, getMcqsByPdf);
+
+airouter.get("/qa/:pdfId", requireAuth, getQaByPdf);
 
 export default airouter;
