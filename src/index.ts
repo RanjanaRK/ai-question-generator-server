e7ebc7f50e433eb6ea74b74676a61c482ae1874f
RanjanaRK <ranjanakumari.me@gmail.com>
2026-02-23 22:49:40 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,13 @@ import app from "./server";
 
 // dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
